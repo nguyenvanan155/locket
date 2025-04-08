0b43b78b1146a68e0180b02f56099c3d051bdbd3
dio <doibncm2003@gmail.com>
2025-04-08 12:46:09 +0700

    edit V0.0.4

--- src/utils/API/apiRoutes.js
+++ src/utils/API/apiRoutes.js
@@ -1,7 +1,7 @@
 //const BASE_API_URL = "https://my-api-locket.vercel.app"; //http://localhost:5004
-const BASE_API_URL = "http://localhost:5004";//https://my-api-locket-production.up.railway.app/
+//const BASE_API_URL = "http://localhost:5004";//https://my-api-locket-production.up.railway.app/
 //const BASE_API_URL = "https://my-api-locket-production.up.railway.app";https://my-api-locket.onrender.com
-//const BASE_API_URL = "https://apilocket-diov2.onrender.com";
+const BASE_API_URL = "https://apilocket-diov2.onrender.com";
 //const BASE_API_URL = "https://api-locket-dio-v2.vercel.app";
 
 const LOCKET_URL = "/locket";
