5d9d4427ac41218118da96b6bf2a51b655bd26ca
dio <doibncm2003@gmail.com>
2025-04-22 20:02:17 +0700

    edit V1.0.7

--- src/components/UI/CaptionCustomes/ThemesCustomes.jsx
+++ src/components/UI/CaptionCustomes/ThemesCustomes.jsx
@@ -1,35 +1,43 @@
 import React from "react";
+import BouncyLoader from "../Loading/Bouncy";
 
 const ThemesCustomes = ({ title = "Chọn preset", presets = [], onSelect }) => {
+  const isLoading = !presets || presets.length === 0;
+
   return (
     <div className="">
       {title && <h2 className="text-md font-semibold text-primary mb-2">{title}</h2>}
       <div className="flex flex-wrap gap-4 pt-2 pb-5 justify-start">
-        {presets.map((preset) => (
-          <button
-            key={preset.id}
-            className="flex flex-col whitespace-nowrap items-center space-y-1 py-2 px-4 btn h-auto w-auto rounded-3xl font-semibold justify-center"
-            style={{
-              background: `linear-gradient(to top, ${preset.top || preset.color_top}, ${preset.bottom || preset.color_bottom})`,
-              color: preset.text || preset.text_color,
-            }}
-            onClick={() =>
-              onSelect(
-                preset.preset_id,
-                preset.icon,
-                preset.top || preset.color_top,
-                preset.bottom || preset.color_bottom,
-                preset.caption || preset.preset_caption,
-                preset.text || preset.text_color,
-                preset.type
-              )
-            }
-          >
-            <span className="text-base">
-              {(preset.icon || "") + " "}{preset.caption || preset.preset_caption || "Caption"}
-            </span>
-          </button>
-        ))}
+        {isLoading ? (
+          <BouncyLoader color="orange" size={30}/>
+        ) : (
+          presets.map((preset) => (
+            <button
+              key={preset.preset_id || preset.id}
+              className="flex flex-col whitespace-nowrap items-center space-y-1 py-2 px-4 btn h-auto w-auto rounded-3xl font-semibold justify-center"
+              style={{
+                background: `linear-gradient(to top, ${preset.top || preset.color_top}, ${preset.bottom || preset.color_bottom})`,
+                color: preset.text || preset.text_color,
+              }}
+              onClick={() =>
+                onSelect(
+                  preset.preset_id,
+                  preset.icon,
+                  preset.top || preset.color_top,
+                  preset.bottom || preset.color_bottom,
+                  preset.caption || preset.preset_caption,
+                  preset.text || preset.text_color,
+                  preset.type
+                )
+              }
+            >
+              <span className="text-base">
+                {(preset.icon || "") + " "}
+                {preset.caption || preset.preset_caption || "Caption"}
+              </span>
+            </button>
+          ))
+        )}
       </div>
     </div>
   );
